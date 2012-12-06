e62ea39cbbac161ef02e93430102449b99f420f9
Jan Muehlemann <jan.muehlemann@gmail.com>
2012-12-06 13:44:17 +0100

    touch fix at least for lvl1 nav dropdown

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
-$.support.placeholder = false;
-var placeholderTest = document.createElement('input');
-if('placeholder' in placeholderTest) $.support.placeholder = true;
+(function() {
+  $.support.placeholder = false;
+  var placeholderTest = document.createElement('input');
+  if('placeholder' in placeholderTest) $.support.placeholder = true;
 
-$(document).ready(function() {	
-	//= brandbar.toggle.js
-	//= sidebar.accordion.toggle.js
-});
+  $(document).ready(function() {  
+    //= brandbar.toggle.js
+    //= sidebar.accordion.toggle.js
+      //= bootstrap.touchfix.js
+  });
 
-//= form.js
-//= equalheight.js
+  //= form.js
+  //= equalheight.js
+})();
